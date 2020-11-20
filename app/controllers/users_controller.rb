cc9410f353658496990b5876227b0a5f42b80e55
hase <mail>
2020-11-20 05:54:25 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,4 +17,3 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :email, :password, :password_confirmation)
   end 
-end   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
   root 'pages#index'
   get 'pages/help'
   
   resources :users
-  
-  get    '/login',  to: 'sessions#new'
-  post   '/login',  to: 'sessions#create'
-  delete '/login',  to: 'sessions#destroy'
-end   
+ end   
