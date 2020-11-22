83c5bf108bc820a10f72ef3d3cd3757e728d71a4
hase <mail>
2020-11-22 01:45:30 +0000

    first commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,19 +1,14 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
+def new
+   @user = User.new
+end
     
-  def create
-   @user = User.new(user_params)
+ def create
+   binding.pry
+   @user = User.new(name: params[:user][:name], email: params[user][email])
    if @user.save
-    redirect_to root_path, success: '登録が完了しました'
+     redirect_to root_path
    else
-    　flash.now[:danger] = "登録に失敗しました"
-    　render :new
+     render :new
    end
-  end 
-
-  private
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end 
+ end
