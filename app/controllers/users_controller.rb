ef33b0b60147b55599f52b3cc31309a541739086
hase <mail>
2020-11-23 01:25:27 +0000

    form_forについて

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,10 @@
 class UsersController < ApplicationController
-def new
-   @user = User.new
-end
+end  
+ def new
+    @user = User.new
+ end
     
  def create
-   binding.pry
    @user = User.new(name: params[:user][:name], email: params[user][email])
    if @user.save
      redirect_to root_path
